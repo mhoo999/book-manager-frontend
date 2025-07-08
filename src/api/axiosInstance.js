f537600ccb7d7a24777238df1016b1a887e85fcc
latte3cup <latte3cup@naver.com>
2025-07-08 15:31:31 +0900

    first

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,39 +1,43 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
   baseURL: '/api',
-  withCredentials: true
-});
+  withCredentials: true,
+})
 
 // 요청 인터셉터, 응답 인터셉터 공통
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('accessToken');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
-});
+  return config
+})
 
 api.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config
 
     if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+      originalRequest._retry = true
 
-      const res = await axios.post('/api/auth/refresh', {}, { withCredentials: true });
-      const newAccessToken = res.data.accessToken;
+      const res = await axios.post(
+        '/api/auth/refresh',
+        {},
+        { withCredentials: true },
+      )
+      const newAccessToken = res.data.accessToken
 
-      localStorage.setItem('accessToken', newAccessToken);
+      localStorage.setItem('accessToken', newAccessToken)
 
-      api.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-      originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`
+      originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
 
-      return api(originalRequest);
+      return api(originalRequest)
     }
-    return Promise.reject(error);
-  }
-);
+    return Promise.reject(error)
+  },
+)
 
-export default api;
+export default api
